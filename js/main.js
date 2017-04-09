9aba77e02331b4248371f138970fb09be95a77d2
Axel <axelfuh@gmail.com>
2017-04-09 16:43:07 +0200

    Working version

--- js/main.js
+++ js/main.js
@@ -1,99 +1,137 @@
 let snake,
-    init,
     food,
     tail,
     grow = [[]],
     canSize = 600,
     canvasColor = 51,
-    square = Math.floor(canSize/25), // number of pseudo squares
-    fps = 10;
+    square = Math.floor(canSize/30), // number of pseudo squares
+    fps = 8;
 
 class Snake {
   constructor(){
-    this.x = 0;
-    this.y = 0;
-    this.xMove = 1;
-    this.yMove = 0;
-    this.total = 0;
-    this.body = [[]];
+      this.pos = createVector(0, 0);
+      this.speed = createVector(1, 0);
+      this.score = 0;
+      this.tail = [];
   }
   direction(move){
-    this.xMove = move[0];
-    this.yMove = move[1];
-  }
-  update(){
-    this.x += this.xMove * square;
-    this.y += this.yMove * square;
-    this.x = constrain(this.x, 0, canSize - square);
-    this.y = constrain(this.y, 0, canSize - square);
+    if(move[0] * this.speed.x < 0) {
+      return;
+    }
+    else if (move[1] * this.speed.y < 0 ){
+      return;
+    }
+
+    this.speed.x = move[0];
+    this.speed.y = move[1];
   }
   eat(foodPosition){
-    const distance = int(dist(this.coords()[0], this.coords()[1], foodPosition[0], foodPosition[1])); //distance between the snake and the food
-    if (distance === 0){
-      this.total++;
-      return true;
-    }
-    else {
-      return false;
-    }
+      const distance = int(dist(this.coords()[0], this.coords()[1], foodPosition[0], foodPosition[1])); //distance between the snake and the food
+      if (distance === 0){
+        this.score++;
+        // fps++;
+        return true;
+      } else {
+        return false;
+      }
   }
   coords(){
-    let coords = [this.x/square, this.y/square];
-    return coords;
+      let coords = [this.pos.x/square, this.pos.y/square];
+      return coords;
+  }
+  death(){
+      if (this.pos.x < 0 || this.pos.y < 0 || this.pos.x >= canSize|| this.pos.y >= canSize){
+        state.death();
+      }
+      for(let i = 0; i < this.tail.length; i++){
+        if(this.tail[i].x == this.pos.x && this.tail[i].y == this.pos.y){
+          state.death();
+        }
+      }
+  }
+  update(){
+      for (let i = 0; i < this.tail.length - 1; i++) {
+        this.tail[i] = this.tail[i + 1];
+      }
+      if (this.score >= 1) {
+        this.tail[this.score - 1] = createVector(this.pos.x, this.pos.y);
+      }
+      this.pos.x += this.speed.x * square;
+      this.pos.y += this.speed.y * square;
+      snake.death();
   }
   render(){
-    const col = color(255, 204, 0);
-    fill(col);
+      const col = color(255, 204, 0);
+      fill(col);
+      noStroke();
 
-    this.body.map((i) => {
-      let head = createVector(this.x, this.y);
-      rect(head.x, head.y, square, square);
-    });
+      for(let i = 0; i < this.tail.length; i++){
+        rect(this.tail[i].x, this.tail[i].y, square, square);
+      }
 
+      rect(this.pos.x, this.pos.y, square, square);
   }
 }
-
 
 class Food{
   constructor() {
-    this.x = Math.floor(random(canSize/square)) * square;
-    this.y = Math.floor(random(canSize/square)) * square;
+      this.x = Math.floor(random(canSize/square)) * square;
+      this.y = Math.floor(random(canSize/square)) * square;
   }
   coords(){
-    let coords = [this.x/square, this.y/square];
-    return coords;
+      let coords = [this.x/square, this.y/square];
+      return coords;
   }
   render(){
-    fill("red");
-    noStroke();
-    rect(this.x, this.y, square, square, 10);
-    this.coords();
+      fill(255);
+      rect(this.x, this.y, square, square);
+      this.coords();
   }
 }
 
+const state = {
+  game: true,
+  run: () => {
+      state.game = true;
+      frameRate(fps);
+      background(canvasColor);
+      snake.render();
+      snake.update();
+      food.render();
+      message.score();
+
+      if (snake.eat(food.coords())){
+        food = new Food();
+      }
+  },
+  death: () => {
+      noLoop();
+      snake.constructor();
+      state.game = false;
+      message.death();
+      fps = 6;
+  },
+  pause: () => {
+      if (!state.game){
+        loop();
+        state.game = true;
+      }
+  }
+};
+
+
+
+//====== Setup =======
 function setup() {
   createCanvas(canSize, canSize).parent("game");
+  stroke(255);
+  noFill();
   snake = new Snake();
   food = new Food();
-  frameRate(fps);
 }
 
 function draw() {
-  background(canvasColor);
-  snake.render();
-  snake.update();
-
-  food.render();
-
-  if (snake.eat(food.coords())){
-    console.log("nom nom");
-    food = new Food();
-    snake.body.push([]);
-  }
-
-  if(pause.check) {
-    pause.text();
-  }
+    state.run();
 }
 
 function keyPressed(){
@@ -109,177 +147,31 @@ function keyPressed(){
   else if(keyCode === LEFT_ARROW){
     snake.direction([-1, 0]);
   }
-  else if(!pause.check && keyCode === 32){
-    pause.true();
-  }
-  else if(pause.check && keyCode === 32){
-    pause.false();
+  else if(keyCode === 32){ // spacebar
+    state.pause ();
   }
 }
 
-const pause = {
-  check: false,
-  true: () => {
-    noLoop();
-    pause.check = true;
-    pause.text();
-  },
-  false: () => {
-    loop();
-    pause.check = false;
-  },
-  text: () => {
-    let msg = "Pause";
-    textSize(40);
+const message = {
+  death: () => {
+    textSize(32);
+    let msg = "game over",
+        msg2 = "press spacebar to play again";
+    let msgW = textWidth(msg),
+        msg2W = textWidth(msg2);
+
     fill(255);
-    text(msg, 480, 600);
+    text(msg, canSize/2 - msg2W/2, canSize/2);
+
+    textSize(30);
+    text(msg2, canSize/2 - msg2W/2, canSize/2 + square);
+  },
+  score: () =>{
+    textSize(22);
+    fill(255);
+    let msg = "score " + snake.score * 100,
+        msgW = textWidth(msg);
+
+    text(msg, 2, canSize);
   }
 };
-
-
-
-
-/*
-function Game(size) {
-    this.size = size;
-}
-
-Game.prototype.grid = function () {
-  let cell = [];
-
-  for (let x = this.size; x > 0; x--) {
-    cell.push([]);
-    $("#grid").prepend("<div id='col" + x + "'" + " " + "class=\"col\"></div>");
-
-    for (let y = this.size; y > 0; y--) {
-      cell.push(" ");
-
-      let cellData = $("<div class=\"cell\"></div>");
-      cellData.attr("data-coord", + x + ", " + y);
-      $("#col" + x + "").prepend(cellData);
-    }
-  }
-  return cell;
-};
-
-
-Game.prototype.coordinates = function () {
-    let input, data;
-
-    $(".cell").mouseenter(function () {
-      data = $(this).data("coord");
-      input = JSON.parse("[" + data + "]");
-      console.log(input);
-    });
-};
-
-Game.prototype.move = function (dir) {
-  let i = 0;
-  let coord = [20, 20];
-  let init;
-  let left = true,
-      right = false,
-      up = true,
-      down = false;
-
-    init = setInterval(function(){
-      if (!right && dir === "left") {
-        coord[0]--;
-      }
-      else if (dir === "up") {
-        coord[1]--;
-      }
-      else if (dir === "right") {
-        coord[0]++;
-      }
-      else if (dir === "down") {
-        coord[1]++;
-      }
-      let pinta = $("div[data-coord='" + coord[0] + ", " + coord[1] + "']");
-      pinta.addClass("negro").delay(200).queue(function(){
-        $(this).removeClass("negro").dequeue();
-});
-
-
-    }, 200);
-
-};
-
-Game.prototype.bind = function () {
-  let coord = [20, 20];
-
-  $(document).on("keydown", function(event){
-    switch(event.which) {
-        case 37: // left //// [-1, igual]
-        case 65: // A
-        Game.prototype.move("left");
-        coord[0]--;
-        break;
-
-        case 38: // up //// [igual, -1]
-        case 87: // W
-        Game.prototype.move("up");
-        coord[1]--;
-        break;
-
-        case 39: // right //// [+1, igual]
-        case 68: // D
-        Game.prototype.move("right");
-        coord[0]++;
-        break;
-
-        case 40: // down //// [igual, +1]
-        case 83:  // Sa
-        Game.prototype.move("down");
-        coord[1]++;
-        break;
-
-        case 32: //bar //// pausa
-        console.log("bar");
-        break;
-
-        default: return;
-    }
-    event.preventDefault();
-
-  });
-};
-
-
-
-
-
-
-// let arr = [[18, 20], [19, 20], [20, 20]];
-// Game.prototype.alarga = function(val) {
-//   val.push(val);
-//   this.snake(arr);
-// };
-//
-//
-// Game.prototype.snake = function (val) {
-//   val.map((x) => {
-//     let pinta = $("div[data-coord='" + x[0] + ", " + x[1] + "']");
-//     pinta.addClass("negro");
-//   });
-// };
-//
-// Game.prototype.move = function(val) {
-//   let right = [];
-//   val.map((x) => {
-//     right.push([x[0] + 1, x[1]]);
-//   });
-//   this.snake(right);
-// };
-//
-//
-$(function(){
-  game.grid();
-  game.bind();
-});
-
-
-
-const game = new Game(40);
-
-*/
