d48b4f7856d9e4873406362d6e2572f99c0900b5
Axel <axelfuh@gmail.com>
2017-04-10 09:23:16 +0200

    Update JS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# snake
+# Snake game
+
+From The Odin Project's [curriculum](http://www.theodinproject.com/courses/javascript-and-jquery/lessons/jquery-and-the-dom)
--- js/main.js
+++ js/main.js
@@ -1,48 +1,49 @@
 let snake,
+    move = [1, 0],
     food,
-    tail,
-    grow = [[]],
     canSize = 600,
     canvasColor = 51,
-    square = Math.floor(canSize/30), // number of pseudo squares
+    square = Math.floor(canSize/30), // That would be a 30x30 grid
     fps = 8;
 
+// =================== Snake ====================
 class Snake {
   constructor(){
-      this.pos = createVector(0, 0);
+      this.pos = createVector(canSize/2, canSize/2); // initial position
       this.speed = createVector(1, 0);
       this.score = 0;
       this.tail = [];
   }
-  direction(move){
-    if(move[0] * this.speed.x < 0) {
-      return;
-    }
-    else if (move[1] * this.speed.y < 0 ){
-      return;
-    }
-
-    this.speed.x = move[0];
-    this.speed.y = move[1];
+  direction(dir){
+      //prevents it from moving in the opposite direction
+      if (dir[0] * this.speed.x < 0) {
+        return;
+      }
+      else if (dir[1] * this.speed.y < 0 ){
+        return;
+      }
+      this.speed.x = dir[0];
+      this.speed.y = dir[1];
   }
-  eat(foodPosition){
-      const distance = int(dist(this.coords()[0], this.coords()[1], foodPosition[0], foodPosition[1])); //distance between the snake and the food
+  eat(foodPosition){ //calculate the distance between the snake and the food
+      const distance = int(dist(this.coords()[0], this.coords()[1], foodPosition[0], foodPosition[1]));
       if (distance === 0){
         this.score++;
-        // fps++;
+        fps++;
         return true;
       } else {
         return false;
       }
   }
-  coords(){
+  coords(){ // Snake's head coordinates
       let coords = [this.pos.x/square, this.pos.y/square];
       return coords;
   }
-  death(){
+  death(){ // If the snake collides with the edge, the game is over.
       if (this.pos.x < 0 || this.pos.y < 0 || this.pos.x >= canSize|| this.pos.y >= canSize){
         state.death();
       }
+      // If the snake collides with its own tail, the game is over.
       for(let i = 0; i < this.tail.length; i++){
         if(this.tail[i].x == this.pos.x && this.tail[i].y == this.pos.y){
           state.death();
@@ -50,45 +51,51 @@ class Snake {
       }
   }
   update(){
+      snake.direction(move);
+      // Create the tail
       for (let i = 0; i < this.tail.length - 1; i++) {
         this.tail[i] = this.tail[i + 1];
       }
       if (this.score >= 1) {
         this.tail[this.score - 1] = createVector(this.pos.x, this.pos.y);
       }
+
       this.pos.x += this.speed.x * square;
       this.pos.y += this.speed.y * square;
-      snake.death();
+
+      snake.death(); // In every "turn" check if the snake dies.
   }
   render(){
-      const col = color(255, 204, 0);
-      fill(col);
+      const snakeColor = color(255, 204, 0);
+      fill(snakeColor);
       noStroke();
 
       for(let i = 0; i < this.tail.length; i++){
-        rect(this.tail[i].x, this.tail[i].y, square, square);
+        rect(this.tail[i].x, this.tail[i].y, square, square); //Create the body
       }
 
-      rect(this.pos.x, this.pos.y, square, square);
+      rect(this.pos.x, this.pos.y, square, square); // Create the head
   }
 }
 
+// =================== Food ====================
 class Food{
   constructor() {
       this.x = Math.floor(random(canSize/square)) * square;
       this.y = Math.floor(random(canSize/square)) * square;
   }
-  coords(){
+  coords(){ //Food coordinates. They are used to check the distance between the food and the snake.
       let coords = [this.x/square, this.y/square];
       return coords;
   }
   render(){
       fill(255);
-      rect(this.x, this.y, square, square);
+      rect(this.x, this.y, square, square); // create food in a random position.
       this.coords();
   }
 }
 
+// =================== Game state ====================
 const state = {
   game: true,
   run: () => {
@@ -106,10 +113,10 @@ const state = {
   },
   death: () => {
       noLoop();
-      snake.constructor();
       state.game = false;
       message.death();
       fps = 6;
+      snake = new Snake();
   },
   pause: () => {
       if (!state.game){
@@ -119,9 +126,7 @@ const state = {
   }
 };
 
-
-
-//====== Setup =======
+// =================== P5 Setup ====================
 function setup() {
   createCanvas(canSize, canSize).parent("game");
   stroke(255);
@@ -136,22 +141,23 @@ function draw() {
 
 function keyPressed(){
   if(keyCode === UP_ARROW) {
-    snake.direction([0, -1]);
+    move = [0, -1];
   }
   else if(keyCode === RIGHT_ARROW){
-    snake.direction([1, 0]);
+    move = [1, 0];
   }
   else if(keyCode === DOWN_ARROW){
-    snake.direction([0, 1]);
+    move = [0, 1];
   }
   else if(keyCode === LEFT_ARROW){
-    snake.direction([-1, 0]);
+    move = [-1, 0];
   }
   else if(keyCode === 32){ // spacebar
     state.pause ();
   }
 }
 
+// =================== Messages ====================
 const message = {
   death: () => {
     textSize(32);
@@ -166,12 +172,11 @@ const message = {
     textSize(30);
     text(msg2, canSize/2 - msg2W/2, canSize/2 + square);
   },
-  score: () =>{
+  score: () => {
     textSize(22);
     fill(255);
     let msg = "score " + snake.score * 100,
         msgW = textWidth(msg);
-
     text(msg, 2, canSize);
   }
 };
